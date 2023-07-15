b011cc535397d3df0f2900e5d25fe6cdfa1b04b1
JerryAgbesi <jerryeagbesi@gmail.com>
2023-07-15 00:23:12 +0000

    set up initializer for loading .env

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"log"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	"github.com/jerryagbesi/gocrud/initializers"
+
 )
 
-func init(){
-	err := godotenv.Load()
 
-	if err != nil {
-	  log.Fatal("Error loading .env file")
-	}
+func init(){
+	initializers.LoadEnvVariables()
+	
 }
 
 func main(){
